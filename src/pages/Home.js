16e902f7d3f5ced9897ef81b275e54b94aede33c
pdjw01 <pdjw01@gmail.com>
2020-08-12 16:14:58 +1000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
+
+const Home = () => {
+  return(
+    <>
+      <Hero>
+        <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting from $299">
+          <Link to='/rooms' className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  )
+}
+export default Home;
